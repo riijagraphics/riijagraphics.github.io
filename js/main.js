cf09c16a4466a169682bbe8ff684f34bc7fa99b5
Simon Schmalfeld <riijagraphics@gmail.com>
2019-11-25 09:00:23 +0900

    Design changes

--- js/main.js
+++ js/main.js
@@ -2,75 +2,93 @@ randomizeBackground();
 createChars($('.title a'), 1000, 0);
 
 function randomizeBackground() {
-    var rn = Math.floor((Math.random() * 150) + 100);
-    var rs = Math.floor((Math.random() * 11) + 4);
-    var t = new Trianglify({
-        x_gradient: Trianglify.colorbrewer.Spectral[rs],
-        noiseIntensity: 0,
-        cellsize: rn
-    });
-    var pattern = t.generate(window.innerWidth, window.innerWidth + 200);
-    document.body.setAttribute('style', 'background-image: ' + pattern.dataUrl);
+  var rn = Math.floor(Math.random() * 150 + 100);
+  var rs = Math.floor(Math.random() * 11 + 4);
+  var t = new Trianglify({
+    x_gradient: Trianglify.colorbrewer.Spectral[rs],
+    noiseIntensity: 0,
+    cellsize: rn
+  });
+  var pattern = t.generate(window.innerWidth, window.innerWidth + 200);
+  document.body.setAttribute('style', 'background-image: ' + pattern.dataUrl);
 }
 
 function createChars(el, duration, minDuration) {
-    var str = $(el).text().trim();
-    $(el).html('');
-    for (var i in str) {
-        var $char = $(document.createElement('span'));
-        $char.addClass('char');
-        $char.text(str[i]);
-        $(el).append($char);
-        $char.css('transition-delay', (minDuration + duration * Math.random()) + 'ms');
-    }
+  var str = $(el)
+    .text()
+    .trim();
+  $(el).html('');
+  for (var i in str) {
+    var $char = $(document.createElement('span'));
+    $char.addClass('char');
+    $char.text(str[i]);
+    $(el).append($char);
+    $char.css(
+      'transition-delay',
+      minDuration + duration * Math.random() + 'ms'
+    );
+  }
 }
 
 function getRandomArbitary(min, max) {
-    return parseInt(Math.random() * (max - min) + min);
+  return parseInt(Math.random() * (max - min) + min);
 }
 
-$('.container, .title a').on('click', function (e) {
-    randomizeBackground();
+$('.container, .title a').on('click', function(e) {
+  randomizeBackground();
 });
 
-$('.container').on('mouseenter', function (e) {
-    var target = $(e.currentTarget).find('.title a');
-    target.addClass('hovered');
+$('.container').on('mouseenter', function(e) {
+  var target = $(e.currentTarget).find('.title a');
+  target.addClass('hovered');
 
-    function move() {
-
-        if (target.hasClass('hovered')) {
-            var $chars = target.find('.char');
-            target.prepend($chars.eq(getRandomArbitary(0, $chars.length)));
-            setTimeout(function () {
-                move();
-            }, 50);
-        }
+  function move() {
+    if (target.hasClass('hovered')) {
+      var $chars = target.find('.char');
+      target.prepend($chars.eq(getRandomArbitary(0, $chars.length)));
+      setTimeout(function() {
+        move();
+      }, 50);
     }
+  }
 
-    move();
+  move();
 });
 
-$('.container').on('mouseleave', function (e) {
-    var target = $(e.currentTarget).find('.title a');
-    target.removeClass('hovered');
-    var text = target.data('text'),
-        $chars = target.find('.char');
+$('.container').on('mouseleave', function(e) {
+  var target = $(e.currentTarget).find('.title a');
+  target.removeClass('hovered');
+  var text = target.data('text'),
+    $chars = target.find('.char');
 
-    var ind = 0;
+  var ind = 0;
 
-    function setChar() {
-        $chars.eq(ind).text(text[ind]);
-        ind++;
-        if (ind < text.length) {
-            setTimeout(function () {
-                setChar();
-            }, 50);
-        }
-    }
-
-    setTimeout(function () {
+  function setChar() {
+    $chars.eq(ind).text(text[ind]);
+    ind++;
+    if (ind < text.length) {
+      setTimeout(function() {
         setChar();
-    }, 100);
+      }, 50);
+    }
+  }
 
+  setTimeout(function() {
+    setChar();
+  }, 100);
 });
+
+function decode(a) {
+  return a.replace(/[a-zA-Z]/g, function(c) {
+    return String.fromCharCode(
+      (c <= 'Z' ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26
+    );
+  });
+}
+
+function openMailer(element) {
+  var y = decode('znvygb:fvzba@evvwn.tencuvpf');
+  element.setAttribute('href', y);
+  element.setAttribute('onclick', '');
+  element.firstChild.nodeValue = 'Loading...';
+}
